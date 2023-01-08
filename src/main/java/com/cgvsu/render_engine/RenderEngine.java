c5f4ad358336e4f4c01a3727d8322f7e3e993a27
Andrew <skid.one@inbox.ru>
2023-01-08 19:26:44 +0300

    UIFix

--- src/main/java/com/cgvsu/render_engine/RenderEngine.java
+++ src/main/java/com/cgvsu/render_engine/RenderEngine.java
@@ -1,6 +1,9 @@
 package com.cgvsu.render_engine;
 
-import com.cgvsu.math.Vector3f;
+import java.util.ArrayList;
+
+import com.cgvsu.math.matrix.floatMatrix.Matrix4f;
+import com.cgvsu.math.vectors.vectorFloat.Vector3f;
 import com.cgvsu.model.ChangedModel;
 import com.cgvsu.rasterization.DrawUtilsJavaFX;
 import com.cgvsu.rasterization.GraphicsUtils;
@@ -8,9 +11,8 @@ import com.cgvsu.rasterization.MyColor;
 import com.cgvsu.rasterization.Rasterization;
 import javafx.scene.canvas.GraphicsContext;
 
-import javax.vecmath.Matrix4f;
+
 import javax.vecmath.Point2f;
-import java.util.ArrayList;
 
 import static com.cgvsu.render_engine.GraphicConveyor.*;
 
@@ -30,8 +32,8 @@ public class RenderEngine {
         Matrix4f projectionMatrix = camera.getProjectionMatrix();
 
         Matrix4f modelViewProjectionMatrix = new Matrix4f(modelMatrix);
-        modelViewProjectionMatrix.mul(viewMatrix);
-        modelViewProjectionMatrix.mul(projectionMatrix);
+        modelViewProjectionMatrix = new Matrix4f(modelViewProjectionMatrix.mul(viewMatrix));
+        modelViewProjectionMatrix = new Matrix4f(modelViewProjectionMatrix.mul(projectionMatrix));
 
 
         final int nPolygons = mesh.getPolygons().size();
@@ -47,7 +49,7 @@ public class RenderEngine {
             for (int vertexInPolygonInd = 0; vertexInPolygonInd < nVerticesInPolygon; ++vertexInPolygonInd) {
                 Vector3f vertex = mesh.getVertices().get(mesh.getPolygons().get(polygonInd).getVertexIndices().get(vertexInPolygonInd));
 
-                javax.vecmath.Vector3f vertexVecmath = new javax.vecmath.Vector3f(vertex.getX(), vertex.getY(), vertex.getZ());
+                Vector3f vertexVecmath = new Vector3f(vertex.getX(), vertex.getY(), vertex.getZ());
 
                 Point2f resultPoint = vertexToPoint(multiplyMatrix4ByVector3(modelViewProjectionMatrix, vertexVecmath), width, height);
                 //вот здесь можно находить максимальную и минимальную координату
@@ -76,7 +78,7 @@ public class RenderEngine {
                         resultPoints.get(vertexInPolygonInd).x,
                         resultPoints.get(vertexInPolygonInd).y);
                 //Rasterization
-                if (vertexInPolygonInd + 1 < nVerticesInPolygon)
+                if (vertexInPolygonInd + 1 < nVerticesInPolygon && mesh.isRasterized())
                     Rasterization.fillTriangle(gr, resultPoints.get(vertexInPolygonInd - 1).x, resultPoints.get(vertexInPolygonInd - 1).y,
                             resultPoints.get(vertexInPolygonInd).x, resultPoints.get(vertexInPolygonInd).y,
                             resultPoints.get(vertexInPolygonInd + 1).x, resultPoints.get(vertexInPolygonInd + 1).y,
